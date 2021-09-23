9fb588a04855ec006037605fa43b793a0a5a8d48
Vladyslav Slipchenko <vladyslav.slipchenko@nixsolutions.com>
2021-09-23 15:37:31 +0300

    Add modules to public api

--- package/src/public-api.ts
+++ package/src/public-api.ts
@@ -1,4 +1,4 @@
 export {RootStore, ChildStore} from './store';
-export {StoreModule} from './modules';
+export {StoreModule, RootStoreModule, ChildStoreModule} from './modules';
 export {Getters, Reducers, Effects, RootStoreConfig, ChildStoreConfig, Immutable} from './models';
 export {castImmutable, safeDeepFreeze} from './utils';
